0878863e68be16c4f5838b61ac28b4ee8ab2e0e2
dameety <djdamilola01@gmail.com>
2016-08-19 00:27:57 +0100

    ammended a few things here

---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('tabs-starter.controllers', [])
+
+  .controller('HomeCtrl', [function($scope) {}])
+
+  .controller('FavouritesCtrl', [function($scope) {}])
+
+  .controller('SettingsCtrl', [function($scope) {}])
